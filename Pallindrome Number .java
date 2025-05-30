cbaed8d040f21a9b24cd20ec6c8216cc2326a036
Ayush chaurasia <ayush.chaurasia_cs24@gla.ac.in>
2025-05-30 16:31:51 +0530

    Create Pallindrome Number .java

---
+++ Pallindrome Number .java
@@ -0,0 +1,26 @@
+
+
+
+
+
+class Solution {
+    public boolean isPalindrome(int x) 
+    {
+        int rev = 0 ;
+        int temp = x ;
+        while (x>0)
+        {
+            int sum = x % 10 ;
+            rev = rev * 10 + sum ;
+            x = x /10 ;
+        }
+        if (rev == temp)
+        {
+            return true ;
+        }
+        else 
+        {
+            return false ; 
+        }
+    }
+}

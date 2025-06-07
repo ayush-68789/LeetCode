c3cb648f7d6e801ab09a10a7fc22ab40932f530b
Ayush chaurasia <ayush.chaurasia_cs24@gla.ac.in>
2025-06-07 19:59:02 +0530

    Create Code.java

---
+++ 283. Move Zeroes to End of the Array/Code.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void moveZeroes(int[] arr) 
+    {
+        int j = 0 ;
+        for (int i = 0 ; i < arr.length ; i++)
+        {
+            if(arr[i] != 0)
+            {
+                arr[j] = arr[i] ;
+                j++ ;
+            }
+        }
+
+        for (int i = j ; i < arr.length ; i++)
+        {
+            arr[i] = 0 ; 
+        }
+        return ;
+    }
+}
